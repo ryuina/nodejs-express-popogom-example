9d5ef286b6785c5c96da15dcd831c9f8d89b05a6
ina.ryu <ina.ryu@navercorp.cp>
2020-12-13 19:58:48 +0900

    Post Api 수정
    - express.json() extend
    - POST /signup api : body로 받아서 없는 항목 있는지 체크크
    - POST /signup/id : id query param으로 받아서 중복여부 체크

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import signup from './signup';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
--- src/signup.ts
+++ src/signup.ts
@@ -13,12 +13,23 @@ router.post('/', (req, res) => {
     return res.status(500).json('Error while signup');
   }
 
-  return res.json({user: req.body});
+  const { id, password, name, phoneAddress, emailAddress, advertisement } = req.body;
+
+  console.info(`[New User Signup]
+id: ${id}, password: ${password}, name: ${name}, phoneAddress: ${phoneAddress}, emailAddress: ${emailAddress}, advertisement: ${advertisement}`);
+
+  if (!(id && password && name && phoneAddress && emailAddress && advertisement)) {
+    return res.json({error: {code: 100, message: '회원가입 폼에 없는 항목이 있습니다.', data: req.body }});
+  }
+  return res.json(req.body);
 });
 
 router.post('/id', (req, res) => {
-  if (req.body.id === 'ryuina') {
-    return res.json({error: {code: 100, message: '중복된 아이디 입니다.'}});
+  const id = req.query.id;
+  console.info(`[Id Check]
+id: ${id}`);
+  if (id === 'ryuina') {
+    return res.json({ error: { code: 200, message: '중복된 아이디 입니다.' }});
   }
   return res.sendStatus(200);
 })
