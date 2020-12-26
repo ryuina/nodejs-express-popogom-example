e68682c5400ecd833e175c9620d77a2a2ac82851
ina.ryu <ina.ryu@navercorp.cp>
2020-12-26 19:07:24 +0900

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import signup from './signup';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 
 app.listen(3000, () => {
